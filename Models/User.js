f7a5b009a354df5799ec7555493bf23a56915465
Ansar <md.atif.ansar@accenture.com>
2024-12-14 18:46:32 +0530

    added UserSchema custom methods, ispasswordcorrect, hash password pre-save

--- Models/User.js
+++ Models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -15,5 +16,15 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password"))
+    this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
